af2f8610d28efe56f38085a954e1db90d05da506
Chezl <ezlvansa_myxc002@yahoo.com>
2016-12-16 06:07:26 +0800

    deleted init.py

--- app/init.py
+++
@@ -1,9 +0,0 @@
-
-from flask import Flask, render_template, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config.from_pyfile('config.py')
-db = SQLAlchemy(app)
-
-from app import views, models
